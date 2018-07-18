3a7efbce398c587e32ead61b48e3c76ec8c80ed9
Anton Khodak <anton.khodak@sanger.ac.uk>
2018-07-18 11:31:33 +0100

    Add plot

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import requests
 import wtforms
 from flask import Flask, render_template, request
+from markupsafe import Markup
 from wtforms import validators
 
-from config import MODEL_HOST
+from config import MODEL_URL
 
 app = Flask(__name__)
 
 
 class MutationalProfileForm(wtforms.Form):
-    seq = wtforms.StringField('Sequence', [validators.DataRequired()])
-    cut_site = wtforms.IntegerField('Cut at', [validators.Length(min=6, max=35),
-                                                  validators.DataRequired()])
+    seq = wtforms.StringField('Target DNA sequence', [validators.DataRequired()])
+    cut_site = wtforms.IntegerField('Index of PAM (1-based)', [validators.DataRequired()])
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -20,10 +20,10 @@ def plot():
     if request.method == 'POST' and form.validate():
         seq = form.seq.data
         cut_site = form.cut_site.data
-        plot_data = requests.post(MODEL_HOST, {"seq": seq,
-                                          "cut_site": cut_site})
+        plot_data = requests.post(MODEL_URL, data={"seq": seq,
+                                                   "cut_site": cut_site})
         plot_html = plot_data.json().get("plot")
-        return render_template("plot.html", plot=plot_html)
+        return render_template("plot.html", plot=Markup(plot_html), form=form)
     return render_template("plot.html", form=form)
 
 
